dae9eb010ac7e605de825f9347c4d5b95df27ce7
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2023-10-18 13:56:41 +1000

    added drawflow deps

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,8 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import Drawflow from 'drawflow';
+import { useEffect } from 'react';
 
 function Hello() {
   return (
@@ -35,11 +37,24 @@ function Hello() {
           </button>
         </a>
       </div>
+      <div id="drawflow" />
     </div>
   );
 }
 
 export default function App() {
+  useEffect(() => {
+    const id = document.getElementById('drawflow');
+    const editor = new Drawflow(id as HTMLElement);
+    editor.start();
+    const html = `
+    <div><input type="text" df-name></div>
+    `;
+    const data = { name: '' };
+    
+    editor.addNode('github', 0, 1, 150, 300, 'github', data, html, false);
+  });
+
   return (
     <Router>
       <Routes>
