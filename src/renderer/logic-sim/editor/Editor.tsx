95728e83c39f404e2f30cd4fc085a2eec30b3bf7
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2023-10-30 16:00:12 +1000

    dependency changes

--- src/renderer/logic-sim/editor/Editor.tsx
+++ src/renderer/logic-sim/editor/Editor.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Dock from "./dock/Dock";
 import { LogicSim } from "./logic-sim";
 import Drawflow from "drawflow";
-import SlidingPanel from "react-sliding-side-panel";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 export default function Editor() {
@@ -16,34 +15,6 @@ export default function Editor() {
     
     return (
         <div>
-            <Sidebar style={{ position:"absolute", height: "100vh" }} collapsed={true}> 
-                <Menu>
-                        <SubMenu label="Inputs"> 
-                            <MenuItem> Constant (On) </MenuItem>
-                            <MenuItem> Constant (Off) </MenuItem>
-                            <MenuItem> Switch </MenuItem>
-                            <MenuItem> Clock </MenuItem>
-                            <MenuItem> Button </MenuItem>
-                            <MenuItem> Delay </MenuItem>
-                        </SubMenu>
-                        <SubMenu label="Outputs"> 
-                            <MenuItem>
-                                <div style={{width: "100%", height: "100px", background:"#333"}}>
-
-                                </div>
-                            </MenuItem>
-                        </SubMenu>
-                        <SubMenu label="Gates">
-                            <MenuItem> AND </MenuItem>
-                            <MenuItem> OR </MenuItem>
-                            <MenuItem> NOT </MenuItem>
-                            <MenuItem> XOR </MenuItem>
-                            <MenuItem> NAND </MenuItem>
-                            <MenuItem> NOR </MenuItem>
-                            <MenuItem> XNOR </MenuItem>
-                        </SubMenu>
-                </Menu>
-            </Sidebar>
             <div id="drawflow-wrapper">
                 {/* <button onClick={() => setOpenPanel(true)}>Open</button> */}
                 <div id="drawflow" />
